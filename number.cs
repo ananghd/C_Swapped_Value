832e3feaad4a7c8cf177c5e52b238af96c0ef747
ananghd <ananghd@gmail.com>
2020-12-22 16:26:15 +0700

    Create number.cs

---
+++ number.cs
@@ -0,0 +1,19 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a = 10;
+		int b = 7;
+    Console.WriteLine("Before Values");
+		Console.WriteLine("a = " + a);
+		Console.WriteLine("b = " + b);
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		Console.WriteLine("After Swapped Values");
+		Console.WriteLine("a = " + a);
+		Console.WriteLine("b = " + b);
+	}
+}
